591f2a61773fc6c27b8beb6fd8fc52182838af49
Phungngoccoc <154677053+Phungngoccoc@users.noreply.github.com>
2024-06-03 21:29:54 +0700

    PHUNGTHENGOC

---
+++ GUI/Quanlynhacungcap.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>giaodientrangchu</title>
+<link rel="stylesheet" href="quanlyhanghoa.css">
+</head>
+<body>
+<header>
+    <h1>QUẢN LÝ NHÀ CUNG CẤP</h1>
+</header>
+<div class="search-bar">
+        <input type="text" placeholder="Tìm kiếm">
+        <button type="submit">Tìm</button>
+    </div>
+<div id="content" style="border: none ;">
+    <iframe name="content-frame" src="" frameborder="0" style="width:100%; height:800px;"></iframe>
+</div>
+</div>
+</body>
+</html>
